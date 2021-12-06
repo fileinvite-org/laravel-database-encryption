19650a66e46f042f5cd1d9a3022bf210547db433
Marvin Quezon <quezon.marvin@gmail.com>
2021-12-06 11:57:12 +0800

    Adds Laravel 6.x support (#1)
    * Updates readme.
    
    * Updates readme.
    
    * Adds Laravel 6.x support.

--- tests/Console/MigrateEncryptionTest.php
+++ tests/Console/MigrateEncryptionTest.php
@@ -10,10 +10,11 @@
 
 namespace AustinHeap\Database\Encryption\Tests\Console;
 
-use AustinHeap\Database\Encryption\Console\Commands\MigrateEncryptionCommand;
-use AustinHeap\Database\Encryption\Tests\DatabaseTestCase;
-use DatabaseEncryption;
 use RuntimeException;
+use DatabaseEncryption;
+use Illuminate\Support\Str;
+use AustinHeap\Database\Encryption\Tests\DatabaseTestCase;
+use AustinHeap\Database\Encryption\Console\Commands\MigrateEncryptionCommand;
 
 /**
  * MigrateEncryptionTest
@@ -33,7 +34,7 @@ class MigrateEncryptionTest extends DatabaseTestCase
         {
             protected function setupKeys()
             {
-                $this->new_key = str_random(32);
+                $this->new_key = Str::random(32);
                 $this->tables  = ['test_models'];
             }
         };
@@ -64,7 +65,7 @@ class MigrateEncryptionTest extends DatabaseTestCase
             protected function setupKeys()
             {
                 $this->old_keys = [app('config')->get('app.key')];
-                $this->new_key  = str_random(32);
+                $this->new_key  = Str::random(32);
             }
         };
 
@@ -81,7 +82,7 @@ class MigrateEncryptionTest extends DatabaseTestCase
             protected function setupKeys()
             {
                 $this->old_keys = [app('config')->get('app.key')];
-                $this->new_key  = str_random(32);
+                $this->new_key  = Str::random(32);
                 $this->tables   = ['test_models'];
             }
         };
